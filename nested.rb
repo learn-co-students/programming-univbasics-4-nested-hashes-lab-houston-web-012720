8fdf243746deaf5443b0a63ec908acf3931ca0e5
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2019-12-30 19:29:33 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-require "pry"
+
 def hopper
 	programmer_hash = 
  		{
@@ -134,7 +134,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     binding.pry
+     
      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
       return  programmer_hash 
 
